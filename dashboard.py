fc8d726fd180440923dc303df5524baa51d37c3b
Maria B <118015089+MariaB91@users.noreply.github.com>
2023-10-08 09:42:11 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,7 +17,10 @@ def main() :
 
     @st.cache_data
     def load_data():
-   
+   #### IMPORTATION DES DONNÉES #####
+# wget.download(
+#     "kaggle datasets download -d mariabouchehboun/merged-data-csv)
+
         data = pd.read_csv('merged_data.csv', index_col='SK_ID_CURR', encoding ='utf-8')
 
         
